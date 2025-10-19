e33b32c221a5ee735df2e17d244c9308114290a4
haunter <test@gmail.com>
2025-10-19 07:41:05 -0700

    Purges more sins away

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1759332090',resources: [
+    cacheName: 'chirpy-1760884837',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
